7c14a4a96fce25156c5ace2fa7145f828ed1c9df
Julien Malard <julien.malard@mail.mcgill.ca>
2015-10-23 08:13:25 -0600

    Reorganización del módulo de redes y de insectos.

--- REDES/REDES.py
+++ REDES/REDES.py
@@ -1,44 +1,64 @@
+import os
+import pylab
+
 from COSO import Coso
-from REDES.INSECTOS import Insecto
 
 
 # Esta clase representa una red agroecológica
 class Red(Coso):
-    def __init__(símismo, nombre, directorio):
+    def __init__(símismo, nombre, insectos, poblaciones_iniciales=None):
+        # insectos debe ser una lista de los objectos de insectos
+
         # El diccionario de los datos para cada red
-        símismo.dic_base = dict(Insectos=[], tipo_ecuaciones="")
-        símismo.ext = "red"  # La extensión para este tipo de documento. (Para guadar y cargar datos.)
-        super().__init__(nombre=nombre, directorio=directorio)  # Esta clase se initializa como describido en Coso
+        símismo.dic = dict(Insectos=insectos, tipo_ecuaciones="")
+
+        # Esta clase se initializa como describido en Coso
+        super().__init__(nombre=nombre, ext='red', directorio=os.path.join('Proyectos', 'Personales', 'Redes'))
+
         símismo.insectos = {}
-        símismo.tipo_ecuaciones = símismo.dic["tipo_ecuaciones"]
         símismo.poblaciones = {}
         símismo.inicializado = False
+        símismo.pobs_inic = poblaciones_iniciales
 
-    def ejec(símismo, poblaciones_iniciales=()):
-        símismo.tipo_ecuaciones = símismo.dic["tipo_ecuaciones"]
+    def ejec(símismo, poblaciones_iniciales=None):
+        # poblaciones_iniciales debe ser un diccionario del formato {'insecto1': {'fase1': pob, 'fase2': pob}, etc.}
+
         for insecto in símismo.dic["Insectos"]:  # Migrar los insectos del diccionario al objeto si mismo
             símismo.insectos[insecto] = símismo.dic["Insectos"][insecto]
-        for insecto in símismo.insectos:  # Crear las instancias de los insectos
+        for insecto in símismo.insectos:  # Inicializar las instancias de los insectos
             símismo.insectos[insecto].ejec(otros_insectos=símismo.dic["Insectos"])
             # Para guardar los datos de poblaciones (para pruebas del modelo)
             símismo.poblaciones[insecto] = {}
             for fase in símismo.insectos[insecto].fases:
                 símismo.poblaciones[insecto][fase] = []
-        if len(poblaciones_iniciales):
-            for insecto in poblaciones_iniciales:  # Inicializar las poblaciones
-                if insecto in símismo.insectos.keys():
+
+        # Inicializar las poblaciones
+        if not poblaciones_iniciales:
+            poblaciones_iniciales = símismo.pobs_inic
+        for insecto in poblaciones_iniciales:
+            if insecto in símismo.insectos:
+                # Si se te olvidó especificar la fase de la población del insecto:
+                if type(poblaciones_iniciales[insecto]) is float or type(poblaciones_iniciales[insecto]) is int:
+                    print('Cuidado: no se especificó la fase para la población inical del insecto %s.' % insecto)
+                    fase = insecto.fases.keys()[0]  # escoger uno al hazar
+                    símismo.insectos[insecto].fases[fase].pob = poblaciones_iniciales[insecto]
+                    símismo.insectos[insecto].fases[fase].hist_pob = [poblaciones_iniciales[insecto]]
+                    # Inicializar los datos de poblaciones de la red
+                    símismo.poblaciones[insecto][fase] = [poblaciones_iniciales[insecto]]
+                else:  # Si no se te olvidó nada
                     for fase in poblaciones_iniciales[insecto]:
-                        # Inicializar la población del insecto
-                        símismo.insectos[insecto].fases[fase].pob = poblaciones_iniciales[insecto][fase]
-                        símismo.insectos[insecto].fases[fase].hist_pob = [poblaciones_iniciales[insecto][fase]]
-                        # Inicializar los datos de poblaciones de la red
-                        símismo.poblaciones[insecto][fase] = [poblaciones_iniciales[insecto][fase]]
-        símismo.inicializado = True
+                        if fase in símismo.insectos[insecto].fases:
+                            # Inicializar la población del insecto
+                            símismo.insectos[insecto].fases[fase].pob = poblaciones_iniciales[insecto][fase]
+                            símismo.insectos[insecto].fases[fase].hist_pob = [poblaciones_iniciales[insecto][fase]]
+                            # Inicializar los datos de poblaciones de la red
+                            símismo.poblaciones[insecto][fase] = [poblaciones_iniciales[insecto][fase]]
+        símismo.inicializado = True  # Marcar la red como inicializada
 
     def incr(símismo, paso, estado_cultivo):
         poblaciones = {}  # Para comunicación con el submódulo PARCELA
         for insecto in símismo.insectos:
-            símismo.insectos[insecto].incr(paso, símismo.tipo_ecuaciones, estado_cultivo)
+            símismo.insectos[insecto].incr(paso, estado_cultivo)
 
         for insecto in símismo.insectos:
             símismo.insectos[insecto].actualizar()
@@ -49,10 +69,13 @@ class Red(Coso):
         return poblaciones  # Para comunicación con el submódulo PARCELA
 
     # Una funcción para simular las plagas en isolación del cultivo (estado del cultivo es un constante exógeno)
-    def simul(símismo, paso, pob_inic, estado_cultivo, tiempo_final, tiempo_inic=0):
-        símismo.borrar(poblaciones_iniciales=pob_inic)
+    def simul(símismo, paso, estado_cultivo, tiempo_final, tiempo_inic=0, pobs_inic=None):
+        if not pobs_inic:
+            pobs_inic = símismo.pobs_inic
+        símismo.borrar(poblaciones_iniciales=pobs_inic)
         for i in range(tiempo_inic, tiempo_final, paso):
             símismo.incr(paso, estado_cultivo=estado_cultivo)
+        símismo.dibujar()
         return símismo.poblaciones
 
     # Una función para borar los datos de simulación (guardando solamente el primer dato, o población inicial)
@@ -67,14 +90,11 @@ class Red(Coso):
         else:
             símismo.ejec(poblaciones_iniciales)
 
-    def dibujar(símismo, insectos=None, datos_obs = None):
-        print("dibujar")
-        import pylab
+    def dibujar(símismo, insectos=None, datos_obs=None):
         if insectos is None:  # Si no se especificaron insectos, utilizar todos los insectos de la red.
             insectos = símismo.insectos
 
         for núm, nombre in enumerate(insectos):
-            print(nombre)
             colores = ("red", "orange", "yellow", "green", "blue", "purple", "fuchsia", "black")
             pylab.figure(1 + núm)  # Un gráfico por insecto (con todas las fases en sub-gráficos)
             pylab.subplots_adjust(hspace=0, right=1)  # Se me olvidó qué hace esta línea...
@@ -91,4 +111,4 @@ class Red(Coso):
                 pylab.ylabel(fase)
                 pylab.xticks = (range(0, int(máx_x*1.1), int(máx_x/10)))
                 pylab.xlabel('Días')
-                pylab.savefig("Tikon_" + nombre + ".png")
+                pylab.savefig("Tikon_%s_%s.png" % (símismo.nombre, nombre))
