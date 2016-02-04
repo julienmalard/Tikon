5309549a22d66d249b9fdd622bba47b8ffbdd149
Julien Malard <julien.malard@mail.mcgill.ca>
2016-02-04 17:36:31 -0500

    Inicio del módulo SIG

--- SIG.py
+++ SIG.py
@@ -1,1 +1,12 @@
 # Código de Judy aquí.
+
+
+class Parcela(object):
+    def __init__(símismo, coordinadas):
+        símismo.coordinadas = coordinadas
+        símismo.área = símismo.calcular_área()
+
+    def calcular_área(símismo):
+        área = 0
+        # Mágia aquí
+        return área
