292f9871e45e5e1ffe729eea863ca9f708f31510
Julien Malard <julien.malard@mail.mcgill.ca>
2016-06-17 23:13:14 -0600

    Progreso

--- instalar.py
+++ instalar.py
@@ -1,9 +1,12 @@
 import os
+import sys
 from subprocess import run
 
 directorio = os.path.split(os.path.realpath(__file__))[0]
 
 directorio_móds = os.path.join(directorio, 'Módulos')
+
+directorio_python = os.path.split(sys.executable)[0]
 
 lista_móds = ['numpy-1.11.0+mkl-cp35-cp35m-win32.whl',
               'matplotlib-1.5.1-cp35-none-win32.whl',
@@ -11,6 +14,7 @@ lista_móds = ['numpy-1.11.0+mkl-cp35-cp35m-win32.whl',
               'pymc-2.3.6-cp35-none-win32.whl']
 
 for mód in lista_móds:
-    comanda = 'pip install %s' % os.path.join(directorio_móds, mód)
+    comanda = '%s install %s' % (os.path.join(directorio_python, 'Scripts', 'pip'),
+                                 os.path.join(directorio_móds, mód))
 
     run(comanda)
