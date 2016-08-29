7c5798561a86a07c17e70fea4a85336682cbfce1
Julien Malard <julien.malard@mail.mcgill.ca>
2016-08-29 12:58:08 -0400

    Primer trabajo en la implementación de un nuevo formato para cultivos, parcelas y paisajes.

---
+++ NuevoPaisaje.py
@@ -0,0 +1,43 @@
+from NuevoCoso import Simulable
+
+
+class Paisaje(Simulable):
+    """
+    
+    """
+
+    ext = '.psj'
+
+    def _prep_obs_exper(símismo, exper):
+        raise NotImplementedError  # Para hacer
+
+    def _procesar_predics_calib(símismo):
+        raise NotImplementedError  # Para hacer
+
+    def incrementar(símismo, paso, i, extrn):
+        raise NotImplementedError  # Para hacer
+
+    def _sacar_líms_coefs_interno(símismo):
+        raise NotImplementedError  # Para hacer
+
+    def _llenar_coefs(símismo, n_rep_parám, calibs, comunes, usar_especificados):
+        raise NotImplementedError  # Para hacer
+
+    def dibujar(símismo, mostrar=True, archivo=None, exper=None):
+        raise NotImplementedError  # Para hacer
+
+    def _prep_predics(símismo, n_pasos, n_rep_parám, n_rep_estoc, n_parcelas):
+        raise NotImplementedError  # Para hacer
+
+    def _procesar_validación(símismo):
+        raise NotImplementedError  # Para hacer
+
+    def _prep_args_simul_exps(símismo, exper, n_rep_estoc, n_rep_paráms):
+        raise NotImplementedError  # Para hacer
+
+    def _acción_añadir_exp(símismo, experimento, corresp):
+        raise NotImplementedError  # Para hacer
+
+    def _sacar_coefs_interno(símismo):
+        raise NotImplementedError  # Para hacer
+    
