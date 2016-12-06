1afe2463cd2d4ea6876ea0818982b5460f44c623
Julien Malard <julien.malard@mail.mcgill.ca>
2016-12-06 16:22:59 -0500

    Más trabajo en el primer ejemplo.

--- Matemáticas/Arte.py
+++ Matemáticas/Arte.py
@@ -158,7 +158,7 @@ def gráfico(matr_predic, título, vector_obs=None, tiempos_obs=None,
         dib.savefig(archivo)
 
 
-def graficar_dists(dists, n=1000, valores=None, rango=None, título=None, archivo=None):
+def graficar_dists(dists, n=10000, valores=None, rango=None, título=None, archivo=None):
     """
     Esta función genera un gráfico de una o más distribuciones y valores.
 
