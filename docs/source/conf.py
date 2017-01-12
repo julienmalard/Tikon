bf8ca35206bc103e4c7b0274034ca98dc6072b16
Julien Malard <julien.malard@mail.mcgill.ca>
2017-01-12 16:16:54 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -162,7 +162,7 @@ html_logo = 'Imágenes/LogoTikon.png'
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #
-html_favicon = 'Imágenes/Logo_favicon.ico'
+html_favicon = 'Imágenes/Logo_favicon.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
