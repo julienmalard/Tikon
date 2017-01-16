3cfdcc5aa6529077bb9d0e9e320dddea76183643
Julien Malard <julien.malard@mail.mcgill.ca>
2017-01-16 10:20:18 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -156,13 +156,13 @@ else:
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-html_logo = 'Imágenes/LogoTikon.png'
+html_logo = u'Imágenes/LogoTikon.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #
-html_favicon = 'Imágenes/Logo_favicon.ico'
+html_favicon = u'Imágenes/Logo_favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
