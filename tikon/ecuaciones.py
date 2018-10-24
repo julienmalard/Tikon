84878300e972e5795b0c0befcfc057b9635c77d5
julienmalard <julien.malard@mail.mcgill.ca>
2018-10-24 13:25:03 -0400

    Progreso restructurando ecuaciones

--- tikon/ecuaciones.py
+++ tikon/ecuaciones.py
@@ -1,48 +1,351 @@
-class ÁrbolEcs(object):
-    def __init__(símismo, nombre, categs):
-        símismo.nombre = nombre
-        símismo.categs = categs
+import numpy as np
 
-    def __str__(símismo):
-        return símismo.nombre
+from .calibs import ÁrbolEcs, CategEc, SubCategEc, Ecuación, Parám
 
+inf = np.inf
 
-class CategEc(object):
-    def __init__(símismo, nombre, subcategs):
-        símismo.nombre = nombre
-        símismo.subcategs = subcategs
+ecs_orgs = ÁrbolEcs(
+    'organismo',
+    categs={
+        CategEc(
+            'Crecimiento',
+            subs={
+                SubCategEc(
+                    'Modif',
+                    ecs={
+                        Ecuación('Nada'),
+                        Ecuación(
+                            'Ninguna',
+                            paráms={
+                                Parám('r', (0, inf))
+                            }
+                        ),
+                        Ecuación(
+                            'Log Normal Temperatura',
+                            paráms={
+                                Parám('t', (0, inf)),
+                                Parám('p', (0, inf))
+                            }
+                        )
+                    }
+                ),
+                SubCategEc(
+                    'Ecuación',
+                    ecs={
+                        Ecuación('Nada'),
+                        Ecuación('Exponencial'),  # El exponencial no tiene parámetros a parte de r
+                        Ecuación(
+                            'Logístico',
+                            paráms={
+                                Parám('K', (0, inf)),
 
-    def __str__(símismo):
-        return símismo.nombre
+                            }
+                        ),
+                        Ecuación(
+                            'Logístico Presa',
+                            paráms={
+                                Parám('K', (0, inf), inter='presa')
+                            }
+                        ),
+                        Ecuación(
+                            'Logístico Depredación',
+                            paráms={
+                                Parám('K', (0, inf), inter='presa')
+                            }
+                        ),
+                        Ecuación(
+                            'Constante',
+                            paráms={
+                                Parám('n', (0, inf))
+                            }
+                        )
+                    }
+                )
+            }
+        ),
+        CategEc(
+            'Depredación',
+            subs={
+                SubCategEc(
+                    'Ecuación',
+                    ecs={
+                        Ecuación('Nada'),
+                        Ecuación(
+                            'Tipo I_Dependiente presa',
+                            paráms={
+                                Parám('a', (0, 1), inter=['presa', 'huésped'])
+                            }
+                        ),
+                        Ecuación(
+                            'Tipo II_Dependiente presa',
+                            paráms={
+                                Parám('a', (0, 1), inter=['presa', 'huésped']),
+                                Parám('b', (0, inf), inter=['presa', 'huésped'])
+                            }
+                        ),
+                        Ecuación(
+                            'Tipo III_Dependiente presa',
+                            paráms={
+                                Parám('a', (0, 1), inter=['presa', 'huésped']),
+                                Parám('b', (0, inf), inter=['presa', 'huésped'])
+                            }
+                        ),
+                        Ecuación(
+                            'Tipo I_Dependiente ratio',
+                            paráms={
+                                Parám('a', (0, 1), inter=['presa', 'huésped'])
+                            }
+                        ),
+                        Ecuación(
+                            'Tipo II_Dependiente ratio',
+                            paráms={
+                                Parám('a', (0, 1), inter=['presa', 'huésped']),
+                                Parám('b', (0, inf), inter=['presa', 'huésped'])
+                            }
+                        ),
+                        Ecuación(
+                            'Tipo III_Dependiente ratio',
+                            paráms={
+                                Parám('a', (0, 1), inter=['presa', 'huésped']),
+                                Parám('b', (0, inf), inter=['presa', 'huésped'])
+                            }
+                        ),
+                        Ecuación(
+                            'Beddington-DeAngelis',
+                            paráms={
+                                Parám('a', (0, 1), inter=['presa', 'huésped']),
+                                Parám('b', (0, inf), inter=['presa', 'huésped']),
+                                Parám('c', (0, inf), inter=['presa', 'huésped'])
+                            }
+                        ),
+                    }
+                )
+            }
+        )
+    }
+)
 
+'Tipo I_Hassell-Varley': {'a': {'límites': (0, inf),
+                                'inter': ['presa', 'huésped']},
+                          'm': {'límites': (0, inf),
+                                'inter': ['presa', 'huésped']}
+                          },
 
-class SubCategEc(object):
-    def __init__(símismo, nombre, ecs):
-        símismo.nombre = nombre
-        símismo.ecs = ecs
+'Tipo II_Hassell-Varley': {'a': {'límites': (0, inf),
+                                 'inter': ['presa', 'huésped']},
+                           'b': {'límites': (0, inf),
+                                 'inter': ['presa', 'huésped']},
+                           'm': {'límites': (0, inf),
+                                 'inter': ['presa', 'huésped']}
+                           },
 
-    def __str__(símismo):
-        return símismo.nombre
+'Tipo III_Hassell-Varley': {'a': {'límites': (0, inf),
+                                  'inter': ['presa', 'huésped']},
+                            'b': {'límites': (0, inf),
+                                  'inter': ['presa', 'huésped']},
+                            'm': {'límites': (0, inf),
+                                  'inter': ['presa', 'huésped']}
+                            },
 
+'Kovai': {'a': {'límites': (0, inf),
+                'inter': ['presa', 'huésped']},
+          'b': {'límites': (0, inf),
+                'inter': ['presa', 'huésped']},
+          }
+},
+},
 
-class Ecuación(object):
+'Muertes': {'Ecuación': {'Nada': {},
 
-    def __init__(símismo, nombre, paráms):
-        símismo.nombre = nombre
-        símismo.paráms = paráms
+'Constante': {'q': {'límites': (0, 1),
+                    'inter': None}
+              },
 
-    def eval(símismo, ):
-        raise NotImplementedError
+'Log Normal Temperatura': {'t': {'límites': (-inf, inf),
+                                 'inter': None},
+                           'p': {'límites': (0, inf),
+                                 'inter': None}
+                           },
 
-    def __str__(símismo):
-        return símismo.nombre
+'Asimptótico Humedad': {'a': {'límites': (0, inf),
+                              'inter': None},
+                        'b': {'límites': (-inf, inf),
+                              'inter': None}
+                        },
+'Sigmoidal Temperatura': {'a': {'límites': (-inf, inf),
+                                'inter': None},
+                          'b': {'límites': (0, inf),
+                                'inter': None}
+                          }
+}
+},
 
+'Edad': {'Ecuación': {'Nada': {},
 
-class Parám(object):
-    def __init__(símismo, nombre, líms, inter=None):
-        símismo.nombre = nombre
-        símismo.líms = líms
-        símismo.inter = inter
+'Días': {},  # No se necesitan coeficientes en este caso
 
-    def __str__(símismo):
-        return símismo.nombre
+'Días grados': {'mín': {'límites': (-inf, inf),
+                        'inter': None},
+                'máx': {'límites': (-inf, inf),
+                        'inter': None}
+                },
+'Brière Temperatura': {'t_dev_mín': {'límites': (-inf, inf),
+                                     'inter': None},
+                       't_letal': {'límites': (-inf, inf),
+                                   'inter': None}
+                       },
+'Logan Temperatura': {'rho': {'límites': (0, 1),
+                              'inter': None},
+                      'delta': {'límites': (0, 1),
+                                'inter': None},
+                      't_letal': {'límites': (-inf, inf),
+                                  'inter': None}
+                      },
+'Brière No Linear Temperatura': {'t_dev_mín': {'límites': (-inf, inf),
+                                               'inter': None},
+                                 't_letal': {'límites': (-inf, inf),
+                                             'inter': None},
+                                 'm': {'límites': (0, inf),
+                                       'inter': None}
+                                 },
+},
+},
+
+'Transiciones': {
+    'Prob': {'Nada': {},
+
+'Constante': {'q': {'límites': (0, 1),
+                    'inter': None}
+              },
+
+'Normal': {'mu': {'límites': (0, inf),
+                  'inter': None},
+           'sigma': {'límites': (0, inf),
+                     'inter': None}
+           },
+'Triang': {'a': {'límites': (0, inf),
+                 'inter': None},
+           'b': {'límites': (0, inf),
+                 'inter': None},
+           'c': {'límites': (0, inf),
+                 'inter': None}
+           },
+'Cauchy': {'u': {'límites': (0, inf),
+                 'inter': None},
+           'f': {'límites': (0, inf),
+                 'inter': None}
+           },
+'Gamma': {'u': {'límites': (0, inf),
+                'inter': None},
+          'f': {'límites': (0, inf),
+                'inter': None},
+          'a': {'límites': (0, inf),
+                'inter': None}
+          },
+'Logística': {'u': {'límites': (0, inf),
+                    'inter': None},
+              'f': {'límites': (0, inf),
+                    'inter': None},
+              },
+'T': {'k': {'límites': (0, inf),
+            'inter': None},
+      'mu': {'límites': (0, inf),
+             'inter': None},
+      'sigma': {'límites': (0, inf),
+                'inter': None}
+      }
+},
+
+'Mult': {'Nada': {},
+
+'Linear': {'a': {'límites': (0, inf),
+                 'inter': None}
+           }
+}
+},
+
+'Reproducción': {
+    'Prob': {'Nada': {},
+
+'Constante': {'a': {'límites': (0, inf),
+                    'inter': None},
+              },
+'Depredación': {'n': {'límites': (0, inf),
+                      'inter': ['presa']}
+                },
+'Normal': {'n': {'límites': (0, inf),
+                 'inter': None},
+           'mu': {'límites': (0, inf),
+                  'inter': None},
+           'sigma': {'límites': (0, inf),
+                     'inter': None}
+           },
+'Triang': {'n': {'límites': (0, inf),
+                 'inter': None},
+           'a': {'límites': (0, inf),
+                 'inter': None},
+           'b': {'límites': (0, inf),
+                 'inter': None},
+           'c': {'límites': (0, inf),
+                 'inter': None}
+           },
+'Cauchy': {'n': {'límites': (0, inf),
+                 'inter': None},
+           'u': {'límites': (0, inf),
+                 'inter': None},
+           'f': {'límites': (0, inf),
+                 'inter': None}
+           },
+'Gamma': {'n': {'límites': (0, inf),
+                'inter': None},
+          'u': {'límites': (0, inf),
+                'inter': None},
+          'f': {'límites': (0, inf),
+                'inter': None},
+          'a': {'límites': (0, inf),
+                'inter': None}
+          },
+'Logística': {'n': {'límites': (0, inf),
+                    'inter': None},
+              'u': {'límites': (0, inf),
+                    'inter': None},
+              'f': {'límites': (0, inf),
+                    'inter': None},
+              },
+'T': {'n': {'límites': (0, inf),
+            'inter': None},
+      'k': {'límites': (0, inf),
+            'inter': None},
+      'mu': {'límites': (0, inf),
+             'inter': None},
+      'sigma': {'límites': (0, inf),
+                'inter': None}
+      }
+}
+},
+
+'Movimiento': {
+    # 'Ecuación': {None: {},
+    #            'Inversa cuadrada': {}
+    #           },
+    # 'Modif': {None: {},
+    #          'Presas': {'p': {}
+    #                   }
+    #        },
+    # 'Mobil': {'Constante': {'m': {}},
+    #          'Temperatura': {'m': {},
+    #                         't': {}
+    #                       },
+    #       'Radiación': {}
+    #       },
+},
+
+'Estoc': {
+    'Dist': {
+'Normal': {'sigma': {'límites': (0, 1),
+                     'inter': None}
+           }
+}
+}
+
+}
