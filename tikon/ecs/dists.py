f0ac186b4d275961ed62a52f850095858f41e296
julienmalard <julien.malard@mail.mcgill.ca>
2018-10-26 16:39:14 -0400

    Progreso en calibraciones

--- tikon/ecs/dists.py
+++ tikon/ecs/dists.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from warnings import warn as avisar
 
 class Dist(object):
     pass
@@ -19,21 +19,33 @@ class DistAnalítica(Dist):
 
 
 class DistTraza(Dist):
-    def __init__(símismo, trz):
+    def __init__(símismo, trz, pesos):
         símismo.trz = trz
+        símismo.pesos = pesos
+
+    def obt_vals(símismo, n):
+        reemplazar = n > len(símismo.trz)
+        if reemplazar:
+            avisar()
+        return np.random.choice(símismo.trz, n, replace=reemplazar, p=símismo.pesos)
 
 
 class DistCalib(Dist):
-    def __init__(símismo, tmñ, val):
-        símismo._traza = np.full(tmñ, val)
+    def __init__(símismo, tmñ_trz, val):
+        símismo._traza = np.full(tmñ_trz, val)
         símismo.val = val
+        símismo.pesos = None
 
     def agregar_pnt(símismo, val, i):
-        símismo.val = val
-        símismo._traza[i] = val
+        trnsf = símismo._transf(val)
+        símismo.val = trnsf
+        símismo._traza[i] = trnsf
 
     def gen_traza(símismo):
-        return DistTraza(símismo._traza)
+        return DistTraza(símismo._traza, pesos=símismo.pesos)
+
+    def _transf(símismo, vals):
+        pass
 
     def __float__(símismo):
         return símismo.val
