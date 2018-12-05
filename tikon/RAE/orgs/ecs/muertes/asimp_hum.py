40fa72cd14cef3f83a81acb0aaff1366f8897423
julienmalard <julien.malard@mail.mcgill.ca>
2018-12-05 10:03:47 -0500

    Ecuaciones de mortalidad natural

---
+++ tikon/RAE/orgs/ecs/muertes/asimp_hum.py
@@ -0,0 +1,24 @@
+from tikon.ecs.árb_mód import Ecuación
+from tikon.ecs.paráms import Parám
+
+class A(Parám):
+    nombre = 'a'
+    líms = (0, None)
+
+class B(Parám):
+    nombre = 'b'
+    líms = (None, None)
+
+
+class AsimptóticoHumedad(Ecuación):
+    """
+    M. P. Lepage, G. Bourgeois, J. Brodeur, G. Boivin. 2012. Effect of Soil Temperature and Moisture on
+    Survival of Eggs and First-Instar Larvae of Delia radicum. Environmental Entomology 41(1): 159-165.
+    """
+
+    nombre = 'Asimptótico Humedad'
+    _cls_ramas = [A, B]
+
+    def __call__(símismo, paso):
+        sobrevivencia = np.maximum(0, np.subtract(1, np.exp(-cf['a'] * (mnjdr_móds['clima.humedad'] - cf['b']))))
+        return np.multiply(pob_etp, (1 - sobrevivencia))
