5369e396a297e5e2759fd0a974fd0e6a97e0bb37
julienmalard <julien.malard@mail.mcgill.ca>
2018-12-10 17:01:59 -0500

    Progreso general

--- tikon/ecs/_utils.py
+++ tikon/ecs/_utils.py
@@ -11,4 +11,5 @@ def líms_compat(líms, ref):
     líms = proc_líms(líms)
     ref = proc_líms(ref)
 
-    return líms[0] >= ref[0] and líms[1] <= ref[1]
+    if not líms[0] >= ref[0] and líms[1] <= ref[1]:
+        raise ValueError('Límites {líms} incompatibles con límites teoréticos {ref}.'.format(líms=líms, ref=ref))
--- tikon/ecs/paráms.py
+++ tikon/ecs/paráms.py
@@ -1,6 +1,7 @@
 class MnjdrParáms(object):
     def __init__(símismo, cosos, paráms):
-        símismo._paráms = {str(pr): pr for pr in paráms}
+        símismo._paráms = {str(pr): NotImplemented for pr in paráms}
+        símismo._cosos = cosos
 
     def __getitem__(símismo, itema):
         return símismo._paráms[str(itema)]
