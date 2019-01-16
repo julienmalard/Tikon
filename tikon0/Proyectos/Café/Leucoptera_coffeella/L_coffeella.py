cc2681e5a3ef4ffb84ab5f6af801a84fad5755fb
julienmalard <julien.malard@mail.mcgill.ca>
2019-01-16 14:57:14 -0500

    Compatibilidad con Tiko'n 2.0

--- tikon0/Proyectos/Café/Leucoptera_coffeella/L_coffeella.py
+++ tikon0/Proyectos/Café/Leucoptera_coffeella/L_coffeella.py
@@ -1,23 +1,39 @@
-import tikon0.RAE.Insecto as Ins
-import tikon0.RAE.Planta as Plt
-from tikon0.Matemáticas.Experimentos import Experimento
-from tikon0.Paisaje.Geog import Lugar
-from tikon0.Proyectos.Café.Leucoptera_coffeella.a_prioris import a_prioris
+from pprint import pprint
+
+from tikon.ecs.aprioris import APrioriDens
+from tikon.estruc.simulador import Simulador, EspecCalibsCorrida
+from tikon.exper.exper import Exper
+from tikon.rae.orgs.insectos import MetamCompleta, Parasitoide
+from tikon.rae.orgs.plantas import Hojas
 from tikon.rae.red_ae import RedAE
-
-# Opciones artísticas
-dib_aprioris = True
-ops_dib = {'incert': None, 'todas_líneas': False}
-
-# Empezamos las cosas serias ahora
-proyecto = 'Café\\Leucoptera_coffeella'
+from tikon.rae.red_ae.obs import ObsPobs
+from tikon0.Proyectos.Café.Leucoptera_coffeella.a_prioris import a_prioris
 
 # Crear objeto planta
-Café = Plt.Hojas('Café', proyecto=proyecto)
-Café.estimar_densidad(rango=(38000e6, 42000e6), certidumbre=0.95)
+Café = Hojas('Café')
+dens = APrioriDens(rango=(38000e6, 42000e6), certidumbre=0.95)
+Café.estim_dens(dens)
+
+# Crear objetos para los insectos de esta red
+L_coffeella = MetamCompleta('L. coffeella')
+L_coffeella.secome(Café, etps_símismo='juvenil')
+
+Parasitoide_larvas = Parasitoide('Parasitoide larvas')
+Parasitoide_larvas.parasita(L_coffeella, etps_entra=['juvenil'], etp_emerg='juvenil')
+
+L_coffeella.activar_ec('Edad', subcateg='Ecuación', ec='Días grados')
+Parasitoide_larvas.activar_ec('Edad', subcateg='Ecuación', ec='Días grados')
+
+# Crear objeto red
+Red_café = RedAE([L_coffeella, Parasitoide_larvas, Café])
+
+# A prioris para la nueva red
+Red_café.espec_aprioris(a_prioris)
+
+# pprint(Red_café.ver_coefs_no_espec())
 
 # Crear objeto "base de datos"
-El_Encanto = Experimento(nombre='Suconusco_Chiapas', proyecto=proyecto)
+El_Encanto = Exper()
 # Datos tomados de
 #    Lomeli-Flores, J. Refugio, Juan F. Barrera, and Julio S. Bernal. 2009. “Impact of Natural Enemies on Coffee
 #       Leafminer Leucoptera Coffeella (Lepidoptera: Lyonetiidae) Population Dynamics in Chiapas, Mexico.”
@@ -25,61 +41,45 @@ El_Encanto = Experimento(nombre='Suconusco_Chiapas', proyecto=proyecto)
 #    Lomeli-Flores, J. Refugio, Juan F. Barrera, and Julio S. Bernal. 2010. “Impacts of Weather, Shade Cover and
 #       Elevation on Coffee Leafminer Leucoptera Coffeella (Lepidoptera: Lyonetiidae) Population Dynamics and
 #       Natural Enemies.” Crop Protection 29 (9): 1039–48. doi:10.1016/j.cropro.2010.03.007.
+pobs = ObsPobs.de_csv(
+    archivo='Suconusco_Chiapas.csv', col_tiempo='Día',
+    corresp={
+        'Juvenil': L_coffeella['juvenil'],
+        'Para_larva': Parasitoide_larvas['juvenil']
+    },
+    factor=900 * 1000
 
-El_Encanto.agregar_pobs(archivo='Suconusco_Chiapas.csv', col_tiempo='Día', factor=900 * 1000)
+)
 # Factor: datos en Suconusco_Chiapas está por larvas/plantas; se asume que hay 1 panta cada 10 m2 (2mx5m).
 # El factor de 100 cambia los datos de individuos/plantas a individuos/hectarias.
 
 
-# Crear objetos para los insectos de esta red
-L_coffeella = Ins.MetamCompleta('L. coffeella', proyecto=proyecto, njuvenil=1)
-L_coffeella.secome(Café, etps_depred='juvenil')
-
-Parasitoide_larvas = Ins.Parasitoide('Parasitoide larvas', proyecto=proyecto)
-Parasitoide_larvas.parasita(L_coffeella, etps_infec=['juvenil'], etp_sale='juvenil')
-
-for org in [L_coffeella, Parasitoide_larvas]:
-    for etp in org.etapas:
-        org.aplicar_ecuación(etapa=etp, tipo_ec={'Edad': {'Ecuación': 'Días grados'}})
-
-# Crear objeto red
-Red_café = RedAE([L_coffeella, Parasitoide_larvas, Café])
-
-Red_café.añadir_exp(El_Encanto,
-                    corresp={'L. coffeella': {'juvenil': ['Juvenil']
-                                              #                                              'huevo': ['Huevo'],
-                                              #                                              'pupa': ['Pupa'],
-                                              #                                              'adulto': ['Adulto']
-                                              },
-                             'Parasitoide larvas': {'juvenil': ['Para_larva']}}
-                    )
-
-# A prioris para la nueva red
-for org in [L_coffeella, Parasitoide_larvas]:
-    try:
-        for a_priori in a_prioris[org.nombre]:
-            org.especificar_apriori(**a_priori)
-    except KeyError:
-        pass
-
-from pprint import pprint
-
-pprint(Red_café.ver_coefs_no_espec())
-
 Finca_el_Encanto = Lugar('El Encanto', lat=14.98916667, long=-91.16527778, elev=480)
-Finca_el_Encanto.observar_mensuales(archivo='Finca_El_Encanto.csv', meses='Meses', años='Años',
-                                    cols_datos={'Precipitación': 'Lluvia',
-                                                'Temperatura máxima': 'Temp máx',
-                                                'Temperatura mínima': 'Temp mín'},
-                                    )
+Finca_el_Encanto.observar_mensuales(
+    archivo='Finca_El_Encanto.csv', meses='Meses', años='Años',
+    cols_datos={'Precipitación': 'Lluvia',
+                'Temperatura máxima': 'Temp máx',
+                'Temperatura mínima': 'Temp mín'},
+)
+
+calibs = EspecCalibsCorrida(aprioris=True)
+
+sim = Simulador(Red_café)
+res = sim.simular(exper=El_Encanto, calibs=calibs, n_rep_estoc=10, n_rep_parám=10)
+print('Valid antes de calib')
+pprint(res.validar())
+res.graficar('antes calib')
+
+# sensib = sim.sensib(exper=El_Encanto, n=10)
+# sensib.graficar('sensib antes calib')
+
+sim.calibrar('El Encanto', exper=El_Encanto)
+
 Red_café.ubicar(Finca_el_Encanto)
 
-Red_café.sensibilidad(nombre='Sensib_sobol', tiempo_final=15, exper=El_Encanto, n=2, dibujar=True)
+res_después = sim.simular(exper=El_Encanto, n_rep_estoc=10, n_rep_parám=10)
+pprint(res_después.validar())
+res_después.graficar('después calib')
 
-Red_café.validar(nombre='Valid antes de calib', exper=El_Encanto, n_rep_parám=10, n_rep_estoc=10, guardar=True)
-
-Red_café.calibrar('Prueba', n_iter=10, quema=0, extraer=1, dibujar=True)
-
-Red_café.validar(exper=El_Encanto, nombre='Valid prueba', n_rep_parám=10, n_rep_estoc=10, guardar=True)
-
-Red_café.sensibilidad(nombre='Sensib_sobol', exper=El_Encanto, n=10, dibujar=True)
+# sensib = sim.sensib(exper=El_Encanto, n=10)
+# sensib.graficar('sensib después calib')
