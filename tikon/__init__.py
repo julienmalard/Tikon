aab7c1c5ec94b14840b018a773d913dd88c3cfef
julienmalard <julien.malard@mail.mcgill.ca>
2019-07-14 17:38:37 +0500

    v 2.0.0

--- tikon/__init__.py
+++ tikon/__init__.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-from .versión import __versión__, __version__
+from pkg_resources import resource_filename
+
+with open(resource_filename('tikon', 'versión.txt')) as archivo_versión:
+    __versión__ = __version__ = archivo_versión.read().strip()
 
 # Numpy nos da un montón de errores que no nos importan en Tiko'n.
 np.warnings.filterwarnings('ignore')
--- tikon/versión.py
+++
@@ -1,5 +0,0 @@
-from pkg_resources import resource_filename
-
-
-with open(resource_filename('tikon', 'versión.txt')) as archivo_versión:
-    __versión__ = __version__ = archivo_versión.read().strip()
