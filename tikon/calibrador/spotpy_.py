b28beb62c2d71a6fc27ea4364142c1b3130a48ee
julienmalard <julien.malard@mail.mcgill.ca>
2019-10-23 16:23:26 -0400

    Reorganizado calibrador SpotPy

---
+++ tikon/calibrador/spotpy_.py
@@ -0,0 +1,217 @@
+import math as mat
+import tempfile
+from warnings import warn as avisar
+
+import numpy as np
+import pandas as pd
+import spotpy as spt
+from tikon.calibrador.calibrador import Calibrador
+
+
+class CalibSpotPy(Calibrador):
+    dists_disp = ['Normal', 'Uniforme', 'LogNormal', 'Chi2', 'Exponencial', 'Gamma', 'Wald', 'Triang']
+
+    def __init__(símismo, func, paráms, calibs, frac_guardar=0.05, args_muestrear=None):
+        símismo.frac_guardar = frac_guardar
+        símismo._args_muestrear = args_muestrear or {}
+        super().__init__(func, paráms=paráms, calibs=calibs)
+
+    def calibrar(símismo, n_iter, nombre):
+
+        temp = tempfile.NamedTemporaryFile('w', encoding='UTF-8', prefix="calibTiko'n_")
+
+        mod_spotpy = ModSpotPy(func=símismo.func, dists=símismo.dists, inversar=símismo.inversar)
+        args = dict(spot_setup=mod_spotpy, dbname=temp.name, parallel='seq', dbformat='csv', save_sim=False)
+        args.update(símismo.args_alg())
+
+        muestreador = símismo.alg_spotpy(**args)
+
+        n = mat.ceil(n_iter * símismo.frac_guardar)
+
+        muestreador.sample(**símismo.args_muestrear(n_iter, n_guardar=n))
+
+        egr_spotpy = pd.read_csv(temp.name + '.csv')
+        temp.close()
+
+        vero = egr_spotpy['like1'].values
+        vero, buenas = símismo.filtrar_res(vero, n)
+
+        if not len(buenas):
+            avisar('No se encontró solución aceptable.')
+            return
+
+        for í, cnx in enumerate(símismo.dists):
+            vals = egr_spotpy['parvar_' + str(í)][buenas].values
+            cnx.guardar_traza(nombre=nombre, vals=vals, pesos=vero)
+
+    @property
+    def inversar(símismo):
+        return False
+
+    @property
+    def alg_spotpy(símismo):
+        raise NotImplementedError
+
+    def args_alg(símismo):
+        return {}
+
+    def args_muestrear(símismo, n_iter, n_guardar):
+        args = {'repetitions': n_iter}
+        args.update(símismo._args_muestrear)
+        return args
+
+    def filtrar_res(símismo, vero, n):
+        buenas = np.argpartition(vero, -n)[-n:]
+        vero = vero[buenas]
+        return vero, buenas
+
+
+class EVM(CalibSpotPy):
+    """Estimación de verosimilitud máxima"""
+    alg_spotpy = spt.algorithms.mle
+
+
+class RS(CalibSpotPy):
+    """Recocido Simulado"""
+    alg_spotpy = spt.algorithms.sa
+
+
+class BDD(CalibSpotPy):
+    """Búsqueda Dimensionada Dinámicamente"""
+    alg_spotpy = spt.algorithms.dds
+
+
+class CMEDZ(CalibSpotPy):
+    """Cadena Markov Evolución Diferencial Z"""
+    alg_spotpy = spt.algorithms.demcz
+
+
+class MC(CalibSpotPy):
+    """Monte Carlo"""
+    alg_spotpy = spt.algorithms.mc
+
+
+class MLH(CalibSpotPy):
+    alg_spotpy = spt.algorithms.lhs
+
+
+class CAACAA(CalibSpotPy):
+    """Colonia de Abejas Artificial Caótica Ajustada por Aptitud"""
+
+    alg_spotpy = spt.algorithms.fscabc
+    inversar = True
+
+
+class CAA(CalibSpotPy):
+    """Colonia de Abejas Artificial"""
+
+    alg_spotpy = spt.algorithms.abc
+    inversar = True
+
+
+class ECBUA(CalibSpotPy):
+    """Evolución Compleja Barajada - Universidad de Arizona"""
+
+    alg_spotpy = spt.algorithms.sceua
+    inversar = True
+
+    def filtrar_res(símismo, vero, n):
+        return vero[-n:], slice(-n, None)
+
+    def args_muestrear(símismo, n_iter, n_guardar):
+        args = {'runs_after_convergence': n_guardar}
+        args.update(super().args_muestrear(n_iter, n_guardar=n_guardar))
+        return args
+
+
+class ERP(CalibSpotPy):
+    """Estimación Robusta de Parámetros"""
+
+    alg_spotpy = spt.algorithms.rope
+
+    def args_alg(símismo):
+        return {'alt_objfun': None}
+
+
+class CMMC(CalibSpotPy):
+    """Cadena Markov Monte Carlo"""
+
+    alg_spotpy = spt.algorithms.mcmc
+
+    def filtrar_res(símismo, vero, n):
+        return vero[-n:], slice(-n, None)
+
+    def args_muestrear(símismo, n_iter, n_guardar):
+        args = {'runs_after_convergence': n_guardar}
+        args.update(super().args_muestrear(n_iter, n_guardar=n_guardar))
+        return args
+
+
+class AMAED(CalibSpotPy):
+    """Algoritmo Metrópolis Adaptivo de Evolución Diferencial"""
+
+    alg_spotpy = spt.algorithms.dream
+
+    def filtrar_res(símismo, vero, n):
+        return vero[-n:], slice(-n, None)
+
+    def args_muestrear(símismo, n_iter, n_guardar):
+        args = {'runs_after_convergence': n_guardar}
+        args.update(super().args_muestrear(n_iter, n_guardar=n_guardar))
+        return args
+
+
+class ModSpotPy(object):
+    def __init__(símismo, func, dists, inversar):
+        símismo.func = func
+        símismo.dists = dists
+        símismo.inversar = inversar
+
+    def parameters(símismo):
+        return spt.parameter.generate(
+            [_gen_var_spotpy(d, 'var_' + str(í)) for í, d in enumerate(símismo.dists)]
+        )
+
+    def simulation(símismo, x):
+        for v, (d, v_prm) in zip(x, símismo.dists.items()):
+            for vl in v_prm:
+                vl.val = d.transf_vals(v)
+
+        return símismo.func()
+
+    def evaluation(símismo):
+        return  # símismo.res
+
+    def objectivefunction(símismo, simulation, evaluation, params=None):
+        return -simulation if símismo.inversar else simulation
+
+
+def _gen_var_spotpy(dist, nmbr_var):
+    nombre_dist = dist.nombre_dist
+    paráms = dist.paráms
+
+    ubic = paráms['loc'] if 'loc' in paráms else 0
+    escl = paráms['scale'] if 'scale' in paráms else 1
+
+    if nombre_dist == 'Chi2':
+        var = spt.parameter.Chisquare(nmbr_var, dt=paráms['df'])
+
+    elif nombre_dist == 'Exponencial':
+        var = spt.parameter.Exponential(nmbr_var, scale=escl)
+
+    elif nombre_dist == 'Gamma':
+        var = spt.parameter.Gamma(nmbr_var, shape=paráms['a'], scale=escl)
+
+    elif nombre_dist == 'LogNormal':
+        var = spt.parameter.logNormal(nmbr_var, mean=ubic, sigma=escl)
+
+    elif nombre_dist == 'Normal':
+        var = spt.parameter.Normal(nmbr_var, mean=ubic, stddev=escl)
+
+    elif nombre_dist == 'Uniforme':
+        var = spt.parameter.Uniform(nmbr_var, low=ubic, high=ubic + escl)
+
+    else:
+        raise ValueError(nombre_dist)
+
+    return var
