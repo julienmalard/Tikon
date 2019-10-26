d70dce574ef894737e05f7c4d7c8dce30593f03f
julienmalard <julien.malard@mail.mcgill.ca>
2019-10-26 19:09:45 -0400

    Estoc funciona

--- tikon/móds/rae/orgs/ecs/estoc/normal.py
+++ tikon/móds/rae/orgs/ecs/estoc/normal.py
@@ -1,10 +1,13 @@
+import numpy as np
+import xarray as xr
 from tikon.ecs.árb_mód import Parám
-from tikon.móds.rae.orgs.ecs._plntll_ec import EcuaciónOrg
+from tikon.móds.rae.orgs.ecs._plntll import EcuaciónOrg
 
 
 class Sigma(Parám):
     nombre = 'sigma'
     líms = (0, 1)
+    unids = None
 
 
 class Normal(EcuaciónOrg):
@@ -15,4 +18,9 @@ class Normal(EcuaciónOrg):
     cls_ramas = [Sigma]
 
     def eval(símismo, paso, sim):
-        return símismo.cf['sigma'] * paso
+        pobs = símismo.pobs(sim)
+
+        estoc = xr.ufuncs.maximum(1, pobs * símismo.cf['sigma'] * paso)
+        estoc[:] = np.random.normal(0, estoc)
+
+        return estoc.round()
