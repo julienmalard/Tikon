259ddbd2847ad29593e11d8870683b7b3db1fb59
julienmalard <julien.malard@mail.mcgill.ca>
2019-10-29 12:33:53 -0400

    Arreglado trans

--- tikon/móds/rae/orgs/ecs/trans/prob/_subcateg.py
+++ tikon/móds/rae/orgs/ecs/trans/prob/_subcateg.py
@@ -1,6 +1,8 @@
 import numpy as np
+from tikon.ecs.árb_mód import EcuaciónVacía
+from tikon.móds.rae.orgs.ecs._plntll import SubcategEcOrg
+from tikon.móds.rae.red.utils import RES_TRANS
 
-from tikon.ecs.árb_mód import SubcategEc, EcuaciónVacía
 from .cauchy import Cauchy
 from .constante import Constante
 from .gamma import Gamma
@@ -8,24 +10,7 @@ from .logística import Logística
 from .normal import Normal
 
 
-class ProbTrans(SubcategEc):
+class ProbTrans(SubcategEcOrg):
     nombre = 'Prob'
     cls_ramas = [Normal, EcuaciónVacía, Cauchy, Constante, Gamma, Logística]
-    _nombre_res = TRANS
-    _eje_cosos = EJE_ETAPA
-
-    def postproc(símismo, paso):
-        trans = símismo.obt_val_res(filtrar=False)
-
-        # Redondear las transiciones calculadas
-        np.floor(trans, out=trans)
-
-        # Quitar los organismos que transicionaron
-        símismo.poner_val_mód(POBS, -trans, rel=True, filtrar=True)
-        símismo.poner_val_res(trans)
-
-    def _imprimir(símismo):
-        print(símismo.obt_val_mód(POBS, filtrar=False)[..., símismo.sim.cohortes.í_etps],
-              np.sum(símismo.sim.cohortes._pobs, axis=0))
-        print(símismo.obt_val_mód(POBS, filtrar=False)[..., símismo.sim.cohortes.í_etps] - np.sum(
-            símismo.sim.cohortes._pobs, axis=0))
+    _nombre_res = RES_TRANS
