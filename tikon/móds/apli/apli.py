0a136499eb5dbbea28759ba521805a0e8a098d2f
julienmalard <julien.malard@mail.mcgill.ca>
2019-10-29 10:16:30 -0400

    Módulo de aplicaciones de productos

--- tikon/móds/apli/apli.py
+++ tikon/móds/apli/apli.py
@@ -1,21 +1,23 @@
 from tikon.estruc.módulo import Módulo
 from tikon.estruc.simul import SimulMódulo
 from tikon.móds.apli.res import ResConcentración, ResDecomp
-from tikon.result.utils import gen_coords_base
 
 
 class Aplicaciones(Módulo):
     nombre = 'aplicaciones'
 
+    def __init__(símismo, productos):
+        super().__init__(productos)
+
     def gen_simul(símismo, simul_exper, vars_interés, ecs):
-        return SimulAplicaciones(simul_exper=simul_exper, ecs=ecs)
+        return SimulAplicaciones(simul_exper=simul_exper, ecs=ecs, vars_interés=vars_interés)
+
+    def requísitos(símismo, controles=False):
+        pass
 
 
 class SimulAplicaciones(SimulMódulo):
-    def __init__(símismo, simul_exper, ecs):
-        coords = gen_coords_base()
-        l_res = [
-            ResDecomp(),
-            ResConcentración()
-        ]
-        super().__init__(Aplicaciones.nombre, resultados=l_res, simul_exper=simul_exper, ecs=ecs)
+    resultados = [ResDecomp, ResConcentración]
+
+    def __init__(símismo, simul_exper, ecs, vars_interés):
+        super().__init__(Aplicaciones.nombre, simul_exper=simul_exper, ecs=ecs, vars_interés=vars_interés)
