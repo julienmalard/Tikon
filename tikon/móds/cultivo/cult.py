b57f60957eaad47b227b0beb6c6b63a15190678c
julienmalard <julien.malard@mail.mcgill.ca>
2019-11-03 09:24:23 -0500

    Cultivo listo

--- tikon/móds/cultivo/cult.py
+++ tikon/móds/cultivo/cult.py
@@ -1,20 +1,62 @@
 from tikon.estruc.módulo import Módulo
+from tikon.estruc.simul import SimulMódulo
+from tikon.móds.cultivo.res import RES_BIOMASA
+from tikon.móds.rae.orgs.plantas.externa import CultivoExterno
+from tikon.móds.rae.red import RedAE
+from tikon.móds.rae.red.utils import RES_DEPR, EJE_VÍCTIMA, EJE_ETAPA, RES_POBS
+from tikon.result.utils import EJE_PARC
+
+from . import res
 
 
 class Cultivo(Módulo):
-    def __init__(símismo, modelos):
-        símismo.modelos = modelos
-        super().__init__()
+    nombre = 'cultivo'
 
-    def incrementar(símismo):
-        pass
+    def gen_simul(símismo, simul_exper, vars_interés, ecs):
+        return SimulCultivo(simul_exper=simul_exper, ecs=ecs, vars_interés=vars_interés)
+
+
+class SimulCultivo(SimulMódulo):
+    resultados = [res.ResBiomasa, res.ResHumedadSuelo]
+
+    def __init__(símismo, simul_exper, ecs, vars_interés):
+        símismo.simuls_parcelas = [prc.gen_simul() for prc in simul_exper.exper.parcelas]
+        símismo.red = simul_exper[RedAE.nombre]
+        símismo.etapas = [etp for etp in símismo.red.etapas if isinstance(etp, CultivoExterno)]
+        símismo.superficies = simul_exper.exper.controles['superficies']
+
+        super().__init__(Cultivo.nombre, simul_exper, ecs, vars_interés)
+
+    def iniciar(símismo):
+        for s in símismo.simuls_parcelas:
+            s.iniciar()
+
+    def incrementar(símismo, paso, f):
+        símismo.aplicar_daño()
+        for s in símismo.simuls_parcelas:
+            s.incrementar(paso, f)
+        símismo.mandar_biomasa()
+
+    def aplicar_daño(símismo):
+
+        depred = símismo.red.obt_valor(RES_DEPR)
+
+        # Daño total (todos herbívoros) por etapa de planta, por hectárea
+        daño = depred.loc[{EJE_VÍCTIMA: símismo.etapas}].sum(dim=EJE_ETAPA) / símismo.superficies
+
+        for prc in símismo:
+            prc.aplicar_daño(daño.loc[{EJE_PARC: prc.parcelas}])
+
+    def mandar_biomasa(símismo):
+        símismo.red.poner_valor(RES_POBS, val=símismo.obt_valor(RES_BIOMASA) * símismo.superficies)
 
     def cerrar(símismo):
-        pass
+        for s in símismo.simuls_parcelas:
+            s.cerrar()
 
-    def _gen_resultados(símismo, n_rep_estoc, n_rep_parám, vars_interés):
-        return
+    def requísitos(símismo, controles=False):
+        return {req for prc in símismo for req in símismo[prc].requísitos(controles)}
 
-
-class ModeloCultivo(object):
-    pass
+    def __iter__(símismo):
+        for prc in símismo.simuls_parcelas:
+            yield prc
