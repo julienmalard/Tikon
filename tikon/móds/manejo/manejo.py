1a9d5a9b016feab0fb829381615a9d2b3d1909eb
julienmalard <julien.malard@mail.mcgill.ca>
2019-11-04 15:54:49 -0500

    Manejo funciona

--- tikon/móds/manejo/manejo.py
+++ tikon/móds/manejo/manejo.py
@@ -1,6 +1,7 @@
-import numpy as np
+from copy import deepcopy
 
 from tikon.estruc.módulo import Módulo
+from tikon.estruc.simul import SimulMódulo
 
 
 class Manejo(Módulo):
@@ -13,31 +14,31 @@ class Manejo(Módulo):
 
         super().__init__()
 
-    def incrementar(símismo):
-        for r in símismo:
-            r(símismo.mnjdr_móds, símismo.tiempo)
-
-    def cerrar(símismo):
-        pass
-
-    def requísitos(símismo, controles):
-        pass
-
-    def _gen_resultados(símismo, n_rep_estoc, n_rep_parám, vars_interés):
-        return
-
-    def __iter__(símismo):
-        for r in símismo.reglas:
-            yield r
+    def gen_simul(símismo, simul_exper, vars_interés, ecs):
+        return SimulManejo(mód=símismo, simul_exper=simul_exper, ecs=ecs, vars_interés=vars_interés)
 
 
 class Regla(object):
     def __init__(símismo, condición, acción):
-        símismo.condición = condición
+        símismo.condición = deepcopy(condición)
         símismo.acción = [acción] if callable(acción) else acción
 
-    def __call__(símismo, mnjdr, tiempo):
-        cond_verdad = símismo.condición(mnjdr, tiempo)
-        if np.any(cond_verdad):
+    def __call__(símismo, sim, paso, f):
+        cond_verdad = símismo.condición(sim, paso, f)
+        if cond_verdad.any():
             for a in símismo.acción:
-                a(mnjdr, reps=cond_verdad)
+                a(sim, donde=cond_verdad)
+
+
+class SimulManejo(SimulMódulo):
+
+    def __init__(símismo, mód, simul_exper, ecs, vars_interés):
+        símismo.reglas = mód.reglas
+        super().__init__(mód, simul_exper=simul_exper, ecs=ecs, vars_interés=vars_interés)
+
+    def requísitos(símismo, controles=False):
+        return {req for rgl in símismo.reglas for req in rgl.requísitos(controles)}
+
+    def incrementar(símismo, paso, f):
+        for r in símismo.reglas:
+            r(sim=símismo, paso=paso, f=f)
