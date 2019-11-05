bbf23ac55ab27d0dea3ebde5e1a11be2fe40e5a2
julienmalard <julien.malard@mail.mcgill.ca>
2019-11-05 13:20:52 -0500

    agregar inter para aplicaciones con etapas

--- tikon/móds/apli/apli.py
+++ tikon/móds/apli/apli.py
@@ -1,6 +1,9 @@
+from tikon.ecs.paráms import Inter
 from tikon.estruc.módulo import Módulo
 from tikon.estruc.simul import SimulMódulo
 from tikon.móds.apli.res import ResConcentración, ResDecomp, ResMortalidad
+from tikon.móds.rae.red import RedAE
+from tikon.móds.rae.red.utils import EJE_ETAPA
 
 
 class Aplicaciones(Módulo):
@@ -22,6 +25,20 @@ class SimulAplicaciones(SimulMódulo):
 
     def __init__(símismo, mód, simul_exper, ecs, vars_interés):
         super().__init__(mód, simul_exper=simul_exper, ecs=ecs, vars_interés=vars_interés)
+
+    def inter(símismo, coso, tipo):
+        if isinstance(tipo, str):
+            tipo = [tipo]
+
+        etps_inter = set()
+        for tp in tipo:
+            if tp == 'etapa':
+                etps_inter.update(símismo.simul_exper[RedAE.nombre].etapas)
+            else:
+                raise ValueError(tipo)
+        inter = [[str(etp.org), str(etp)] for etp in etps_inter]
+        if len(inter):
+            return Inter(inter, eje=EJE_ETAPA)
 
     def requísitos(símismo, controles=False):
         pass
