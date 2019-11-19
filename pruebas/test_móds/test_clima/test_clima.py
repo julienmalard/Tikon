2a869bfdb10d36f7776d9b58041ee264a6c86d06
julienmalard <julien.malard@mail.mcgill.ca>
2019-11-19 12:59:07 -0500

    Prueba fecha falta

--- pruebas/test_móds/test_clima/test_clima.py
+++ pruebas/test_móds/test_clima/test_clima.py
@@ -24,3 +24,16 @@ class PruebaClima(unittest.TestCase):
             res_precip.datos_t,
             pd.DataFrame(datos).set_index('tiempo').to_xarray()['precip'].broadcast_like(res_precip.datos_t)
         )
+
+    def test_fecha_falta(símismo):
+        parc = Parcela('parc')
+        lat, lon = parc.geom.centroide
+        f_inic, f_final = '2000-01-01', '2000-02-01'
+        eje_fechas = pd.date_range(f_inic, f_final, freq='D')
+        datos = {'precip': np.random.random(eje_fechas.size), 'tiempo': eje_fechas}
+        fuentes = fnt_json(datos, lat, lon, بلندی=0, تبديل_عمودی_ستون={'precip': 'بارش', 'tiempo': 'تاریخ'})
+        clima = Clima(fuentes=(fuentes,), variables=['precip'])
+        with símismo.assertRaises(ValueError):
+            Modelo(clima).simular(
+                'clima', exper=Exper('exper', parc), t=Tiempo(f_inic, '2000-02-02'), vars_interés=True
+            )
