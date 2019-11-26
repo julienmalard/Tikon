e544faad9f4e45e47cf7094e7219ac52228fab0e
julienmalard <julien.malard@mail.mcgill.ca>
2019-11-25 20:08:16 -0500

    ¡Se nos olvidó T y Triang!

--- tikon/móds/rae/orgs/ecs/trans/prob/_subcateg.py
+++ tikon/móds/rae/orgs/ecs/trans/prob/_subcateg.py
@@ -7,9 +7,11 @@ from .constante import Constante
 from .gamma import Gamma
 from .logística import Logística
 from .normal import Normal
+from .t import T
+from .triang import Triang
 
 
 class ProbTrans(SubcategEcOrg):
     nombre = 'Prob'
-    cls_ramas = [Normal, EcuaciónVacía, Cauchy, Constante, Gamma, Logística]
+    cls_ramas = [Normal, EcuaciónVacía, Cauchy, Constante, Gamma, Logística, T, Triang]
     _nombre_res = RES_TRANS
